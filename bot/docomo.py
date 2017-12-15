3cfaee06203d897e7cb619d6915629e28734223f
Ryt <srpkdyy@gmail.com>
2017-12-15 21:23:41 +0900

    fix

--- bot/docomo.py
+++ bot/docomo.py
@@ -10,7 +10,7 @@ HEADER = {
 }
 
 def reply(request_text, username):
-    reply = "test"
+    reply = (str)username
     return reply
     
 
--- bot/views.py
+++ bot/views.py
@@ -2,13 +2,11 @@ import os
 import json
 import random
 import requests
-import logging
 
 from . import docomo
 from django.shortcuts import render
 from django.http import HttpResponse
 
-logger = logging.getLogger(__name__)
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 HEADER = {
@@ -38,8 +36,8 @@ def parse_json(request_json):
 def get_username(request):
     res_json = requests.get(USER_INFO + request, headers=HEADER)
 #    res = parse_json(res_json)
-    logger.info(res_json)
-    return res_json["displayName"]
+#    return res_json["displayName"]
+    return res_json
 
 def reply_message(reply_token, reply):
     reply_body = {
