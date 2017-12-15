63a84a7f8fdd79656d1c3e4d7d247d28e205eecd
Ryt <srpkdyy@gmail.com>
2017-12-15 20:18:58 +0900

    fix docomo.py

--- bot/docomo.py
+++ bot/docomo.py
@@ -2,7 +2,6 @@ import os
 import json
 import random
 import requests
-import docomo
 
 DOCOMO_ENTRYPOINT = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY='
 DOCOMO_ACCESS_TOKEN = os.getenv("DOCOMO_API_KEY")
@@ -15,5 +14,5 @@ def reply(request_text, username):
     return reply
     
 
-def make_request(reply_token, reply):
+# def make_request(reply_token, reply):
 
